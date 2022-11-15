6e317617f668046da5e016687125561f88363905
deeonwuli <onwulidumebi@gmail.com>
2022-11-15 09:52:20 +0100

    allow multiple selection + dropdown for language wip

--- app.js
+++ app.js
@@ -1,158 +1,221 @@
+var TallySheets = angular.module("TallySheets", [
+  "ngResource",
+  "pascalprecht.translate",
+]);
 
-var TallySheets = angular.module('TallySheets', ['ngResource', 'pascalprecht.translate']);
+var dhisUrl = window.location.href.split("api/apps/")[0];
+var ApiUrl = dhisUrl + "api";
 
-var dhisUrl = window.location.href.split('api/apps/')[0] ;
-var ApiUrl = dhisUrl + 'api';
+TallySheets.controller("TallySheetsController", [
+  "$scope",
+  "$rootScope",
+  "DataSetsUID",
+  "DataSetEntryForm",
+  function ($scope, $rootScope, DataSetsUID, DataSetEntryForm) {
+    var dsSelectorLastId = -1;
+    $scope.dsSelectorList = [];
 
-TallySheets.controller('TallySheetsController', [ "$scope", "DataSetsUID", "DataSetEntryForm", function($scope, DataSetsUID, DataSetEntryForm){
+    // Set "headers" to true by default
+    $scope.headers = true;
 
-	var dsSelectorLastId = -1;
-	$scope.dsSelectorList = [];
+    $scope.addDatasetSelector = function () {
+      dsSelectorLastId++;
+      $scope.dsSelectorList.push({ id: dsSelectorLastId, dataset: {} });
+    };
 
-	// Set "headers" to true by default
-	$scope.headers = true;
+    $scope.deleteDatesetSelector = function (selectPosition) {
+      $scope.dsSelectorList.splice(selectPosition, 1);
+    };
 
-	$scope.addDatasetSelector = function(){
-		dsSelectorLastId++;
-		$scope.dsSelectorList.push({id: dsSelectorLastId, dataset:{}});
-	}
+    $scope.exportToTable = function (tableId) {
+      var uri = "data:application/vnd.ms-excel;base64,",
+        template =
+          '<html xmlns:o="urn:schemas-microsoft-com:office:office" ' +
+          'xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="UTF-8">' +
+          "<!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}" +
+          "</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet>" +
+          "</x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>",
+        base64 = function (s) {
+          return window.btoa(unescape(encodeURIComponent(s)));
+        },
+        format = function (s, c) {
+          return s.replace(/{(\w+)}/g, function (m, p) {
+            return c[p];
+          });
+        };
 
-	$scope.deleteDatesetSelector = function(selectPosition){
-		$scope.dsSelectorList.splice(selectPosition, 1);
-	}
+      var table = $("#" + tableId).clone();
 
-	$scope.exportToTable = function(tableId) {
-		var uri = 'data:application/vnd.ms-excel;base64,'
-			, template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" ' +
-				'xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="UTF-8">' +
-				'<!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}' +
-				'</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet>' +
-				'</x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-			, base64 = function (s) {
-				return window.btoa(unescape(encodeURIComponent(s)))
-			}
-			, format = function (s, c) {
-				return s.replace(/{(\w+)}/g, function (m, p) {
-					return c[p];
-				})
-			}
+      // Remove non-printable section from the table
+      table.find(".hidden-print").remove();
+      table.find(".ng-hide").remove();
 
-		var table = $("#" + tableId).clone();
+      // Replace input fields with their values (for correct excel formatting)
+      table.find("input").each(function () {
+        var value = $(this).val();
+        $(this).replaceWith(value);
+      });
 
-		// Remove non-printable section from the table
-		table.find(".hidden-print").remove();
-		table.find(".ng-hide").remove();
+      // Add border to section table (for printing in MS Excel)
+      table.find(".sectionTable").prop("border", "1");
 
-		// Replace input fields with their values (for correct excel formatting)
-		table.find("input").each(function(){
-			var value = $(this).val();
-			$(this).replaceWith(value);
-		});
+      // Take the name of the first dataset as filename
+      var name = table.find("h2").first().html() + ".xls";
 
-		// Add border to section table (for printing in MS Excel)
-		table.find(".sectionTable").prop('border','1');
+      var ctx = {
+        worksheet: "MSF-OCBA HMIS" || "Worksheet",
+        table: table.html(),
+      };
 
-		// Take the name of the first dataset as filename
-		var name = table.find("h2").first().html() + '.xls';
+      // Create a fake link to download the file
+      var link = angular.element('<a class="hidden" id="idlink"></a>');
+      link.attr({
+        href: uri + base64(format(template, ctx)),
+        target: "_blank",
+        download: name,
+      });
+      $("body").prepend(link[0].outerHTML);
+      $("#idlink")[0].click();
+      $("#idlink")[0].remove();
+    };
 
-		var ctx = {worksheet: 'MSF-OCBA HMIS' || 'Worksheet', table: table.html()}
+    $scope.goHome = function () {
+      window.location.replace(dhisUrl);
+    };
 
-		// Create a fake link to download the file
-		var link = angular.element('<a class="hidden" id="idlink"></a>');
-		link.attr({
-			href: uri + base64(format(template, ctx)),
-			target: '_blank',
-			download: name
-		});
-		$("body").prepend(link[0].outerHTML);
-		$("#idlink")[0].click();
-		$("#idlink")[0].remove();
+    $scope.id = "dsSelector" + $scope.selectorId;
 
-	}
+    $scope.selectorLoaded = false;
 
-	$scope.goHome = function(){
-	  	window.location.replace(dhisUrl);
-	};
+    DataSetsUID.get().$promise.then(function (result) {
+      $scope.dataSetList = result.dataSets.filter((ds) => {
+        var visible = true;
 
-	// Initialize the app with one dataset selector
-	$scope.addDatasetSelector();
-
-}]);
-
-TallySheets.factory("DataSetsUID",['$resource', function ($resource) {
-	return $resource( ApiUrl + '/dataSets.json',
-		{},
-		{ get: { method: "GET",
-		params: {
-			fields: 'id,displayName,attributeValues[value,attribute]',
-			
-			translate: 'true',
-			paging: false
-		}}
-	
-	
-		});
-
-		  
-}]);
-
-TallySheets.factory("DataSetEntryForm",['$resource', function ($resource) {
-	return $resource( dhisUrl + "dhis-web-dataentry/loadForm.action",
-		{ dataSetId:'@dataSetId' },
-		{ get: { method: "GET", transformResponse: function (response) {
-			return {codeHtml: response};}
-		}
-	});
-}]);
-
-TallySheets.directive('onFinishRender', function ($timeout) {
-return {
-    restrict: 'A',
-    link: function (scope, element, attr) {
-        if (scope.$last === true) {
-            $timeout(function () {
-                scope.$emit('ngRepeatFinished');
-            });
+        for (att in ds.attributeValues) {
+          if (
+            ds.attributeValues[att].value == "true" &&
+            ds.attributeValues[att].attribute.id == "Re2UlY7OGO4"
+          ) {
+            visible = false;
+          }
         }
-    }
-}});
+        return visible;
+      });
+    });
 
-TallySheets.directive('d2Progressbar', function(){
-	return{
-		restrict: 'E',
-		templateUrl: 'directives/progressBar/progressBar.html'
-	};
+    $scope.$on("ngRepeatFinished", function (ngRepeatFinishedEvent) {
+      // Refresh bootstrap-select
+      $(".selectpicker").selectpicker("refresh");
+      $(".selectpicker").selectpicker("render");
+      $scope.selectorLoaded = true;
+    });
+
+    $(document).on("change", "#" + $scope.id, function () {
+      var dsId = $("option:selected", this).val();
+      var dsName = $("option:selected", this).html().trim();
+
+      $scope.bindToDataset.id = dsId;
+      $scope.bindToDataset.name = dsName;
+
+      $rootScope.$apply();
+    });
+
+    // Initialize the app with one dataset selector
+    $scope.addDatasetSelector();
+  },
+]);
+
+TallySheets.factory("DataSetsUID", [
+  "$resource",
+  function ($resource) {
+    return $resource(
+      ApiUrl + "/dataSets.json",
+      {},
+      {
+        get: {
+          method: "GET",
+          params: {
+            fields:
+              "id,displayName,translations,attributeValues[value,attribute]",
+            translate: "true",
+            paging: false,
+          },
+        },
+      }
+    );
+  },
+]);
+
+TallySheets.factory("DataSetEntryForm", [
+  "$resource",
+  function ($resource) {
+    return $resource(
+      dhisUrl + "dhis-web-dataentry/loadForm.action",
+      { dataSetId: "@dataSetId" },
+      {
+        get: {
+          method: "GET",
+          transformResponse: function (response) {
+            return { codeHtml: response };
+          },
+        },
+      }
+    );
+  },
+]);
+
+TallySheets.directive("onFinishRender", function ($timeout) {
+  return {
+    restrict: "A",
+    link: function (scope, element, attr) {
+      if (scope.$last === true) {
+        $timeout(function () {
+          scope.$emit("ngRepeatFinished");
+        });
+      }
+    },
+  };
+});
+
+TallySheets.directive("d2Progressbar", function () {
+  return {
+    restrict: "E",
+    templateUrl: "directives/progressBar/progressBar.html",
+  };
 });
 
 TallySheets.config(function ($translateProvider) {
+  $translateProvider.useStaticFilesLoader({
+    prefix: "languages/",
+    suffix: ".json",
+  });
 
-	  $translateProvider.useStaticFilesLoader({
-        prefix: 'languages/',
-        suffix: '.json'
+  $translateProvider.registerAvailableLanguageKeys(["es", "fr", "en", "pt"], {
+    "en*": "en",
+    "es*": "es",
+    "fr*": "fr",
+    "pt*": "pt",
+    "*": "en", // must be last!
+  });
+
+  $translateProvider.fallbackLanguage(["en"]);
+
+  jQuery
+    .ajax({
+      url: ApiUrl + "/userSettings/keyUiLocale/",
+      contentType: "text/plain",
+      method: "GET",
+      dataType: "text",
+      async: false,
+    })
+    .success(function (uiLocale) {
+      if (uiLocale == "") {
+        $translateProvider.determinePreferredLanguage();
+      } else {
+        $translateProvider.use(uiLocale);
+      }
+    })
+    .fail(function () {
+      $translateProvider.determinePreferredLanguage();
     });
-
-	  $translateProvider.registerAvailableLanguageKeys(
-			    ['es', 'fr', 'en', 'pt'],
-			    {
-			        'en*': 'en',
-			        'es*': 'es',
-					'fr*': 'fr',
-					'pt*': 'pt',
-			        '*': 'en' // must be last!
-			    }
-			);
-
-	  $translateProvider.fallbackLanguage(['en']);
-
-	  jQuery.ajax({ url: ApiUrl + '/userSettings/keyUiLocale/', contentType: 'text/plain', method: 'GET', dataType: 'text', async: false}).success(function (uiLocale) {
-		  if (uiLocale == ''){
-			  $translateProvider.determinePreferredLanguage();
-		  }
-		  else{
-			  $translateProvider.use(uiLocale);
-		  }
-    }).fail(function () {
-  	  $translateProvider.determinePreferredLanguage();
-	  });
-
 });
