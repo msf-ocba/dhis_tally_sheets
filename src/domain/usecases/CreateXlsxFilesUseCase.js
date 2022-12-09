0f02f47d086feb577e6a07611b8bd93f6b66cdf3
p3rcypj <pablo@percypj.com>
2022-12-09 03:47:21 +0000

    Filter, order, map categoryCombos correctly

--- src/domain/usecases/CreateXlsxFilesUseCase.js
+++ src/domain/usecases/CreateXlsxFilesUseCase.js
@@ -3,29 +3,30 @@ export class CreateXlsxFilesUseCase {
 
 	execute(dataSets) {
 		const files$ = dataSets.flatMap((dataSet) => {
-			switch (dataSet.formType) {
-				case "DEFAULT":
-					return [
-						{
-							name: `${dataSet.title}.xlsx`,
-							blob: XlsxPopulate.fromBlankAsync().then(
-								(workbook) =>
-									exportDefaultDataSet(workbook, dataSet)
-							),
-						},
-					];
-				default:
-					return [];
-			}
+			if (
+				dataSet.formType === "SECTION" ||
+				dataSet.formType === "DEFAULT"
+			) {
+				return [
+					{
+						name: `${dataSet.name}.xlsx`,
+						blob: XlsxPopulate.fromBlankAsync().then((workbook) =>
+							exportDataSet(workbook, dataSet)
+						),
+					},
+				];
+			} else return [];
 		});
 		return Promise.all(files$);
 	}
 }
 
-function exportDefaultDataSet(workbook, dataSet) {
+function exportDataSet(workbook, dataSet) {
 	const sheet = workbook.sheet(0);
 	sheet.name("MSF-OCBA HMIS");
-	populate(sheet, dataSet);
+	console.log(dataSet);
+	if (dataSet.formType === "DEFAULT") populateDefault(sheet, dataSet);
+	else if (dataSet.formType === "SECTION") populateSections(sheet, dataSet);
 
 	return workbook.outputAsync().then((buffer) => {
 		return new Blob([buffer], {
@@ -40,12 +41,43 @@ function populateHeaders(sheet, header) {
 	sheet.cell("A3").value(header.dataSetName);
 }
 
-function populate(sheet, dataSet) {
+function populateDefault(sheet, dataSet) {
 	if (dataSet.headers) populateHeaders(sheet, dataSet.headers);
-	sheet.cell("A4").value(dataSet.description);
+	sheet.cell("A4").value(dataSet.displayFormName);
 	sheet.cell("B6").value("Value");
 	_.orderBy(
 		dataSet.dataElements,
 		({ displayFormName }) => displayFormName
 	).forEach((de, idx) => sheet.cell(`A${7 + idx}`).value(de.displayFormName));
+}
+
+function populateSections(sheet, dataSet) {
+	if (dataSet.headers) populateHeaders(sheet, dataSet.headers);
+	let row = 3;
+	_.range(0, dataSet.sections.length).map((i) => {
+		const section = dataSet.sections[i];
+		row = addSection(sheet, section, row);
+	});
+}
+
+function addSection(sheet, section, row) {
+	sheet.cell(`A${++row}`).value(section.displayName);
+	if (section.description) sheet.cell(`A${++row}`).value(section.description);
+	++row;
+	section.categoryCombos.forEach((categoryCombo) => {
+		categoryCombo.categories.forEach((category) => {
+			++row;
+			categoryCombo.categoryOptionCombo.forEach(
+				(categoryOptionCombo, idx) => {
+					const column = String.fromCharCode(idx + 66);
+					// sheet.cell(`${column}${row}`).value(categoryOptionCombo.categories??"");
+				}
+			);
+		});
+	});
+	// _.orderBy(
+	// 	dataSet.dataElements,
+	// 	({ displayFormName }) => displayFormName
+	// ).forEach((de, idx) => sheet.cell(`A${7 + idx}`).value(de.displayFormName));
+	return row;
 }
--- src/domain/usecases/GetSelectedDataSetsUseCase.js
+++ src/domain/usecases/GetSelectedDataSetsUseCase.js
@@ -6,31 +6,87 @@ export class GetSelectedDataSetsUseCase {
 	async execute($resource, dataSetsIds) {
 		return await this.dhisRepository
 			.get($resource, dataSetsIds)
-			.$promise.then(({ dataSets }) => {
-				const customDataSets = dataSets.filter(
-					(dataset) => dataset.formType === "CUSTOM"
-				);
-				const sectionedDataSets = dataSets.filter(
-					(dataset) => dataset.formType === "SECTION"
-				);
-				const defaultDataSets = dataSets.filter(
-					(dataset) => dataset.formType === "DEFAULT"
-				);
-
-				const defaultSections = defaultDataSets.map((dataSet) => ({
-					id: dataSet.id,
-					title: dataSet.name,
-					description: dataSet.displayFormName,
-					formType: dataSet.formType,
-					dataElements: dataSet.dataSetElements.map(
-						({ dataElement: de }) => ({
-							id: de.id,
-							displayFormName: de.displayFormName,
-						})
-					),
-				}));
-
-				return [...defaultSections];
-			});
+			.$promise.then(({ dataSets }) =>
+				dataSets.flatMap((dataSet) => {
+					if (dataSet.formType === "CUSTOM") return [];
+					const mappedDataSets = {
+						...dataSet,
+						sections: dataSet.sections.map(mapSection),
+					};
+					return [mappedDataSets];
+				})
+			);
 	}
+}
+
+function mapSection(section) {
+	const mappedCategoryCombos = section.categoryCombos.map((categoryCombo) => {
+		const categoryOptionCombos = categoryCombo.categoryOptionCombos.map(
+			(categoryOptionCombo) => ({
+				...categoryOptionCombo,
+				categories: categoryOptionCombo.displayFormName.split(", "),
+			})
+		);
+
+		const categories = categoryCombo.categories.map(({ categoryOptions }) =>
+			categoryOptions.map(({ displayFormName }) => displayFormName)
+		);
+
+		const dataElements = section.dataElements.filter(
+			(de) => de.categoryCombo.id === categoryCombo.id
+		);
+
+		return {
+			...categoryCombo,
+			categoryOptionCombos,
+			categories,
+			dataElements,
+		};
+	});
+
+	const orderedCategoryCombos = _.orderBy(
+		mappedCategoryCombos,
+		(categoryCombo) =>
+			section.dataElements.findIndex(
+				(de) => de.categoryCombo.id === categoryCombo.id
+			)
+	);
+
+	const categoryCombos = orderedCategoryCombos.map((categoryCombo) => {
+		return {
+			...categoryCombo,
+			categoryOptionCombos: categoryCombo.categoryOptionCombos.sort(
+				(a, b) => {
+					const aPrio = a.categories.map((category) =>
+						getPrio(categoryCombo, category)
+					);
+					const bPrio = b.categories.map((category) =>
+						getPrio(categoryCombo, category)
+					);
+
+					let index = 0;
+					let order = 0;
+					while (
+						order == 0 &&
+						index < categoryCombo.categories.length
+					) {
+						if (aPrio[index] < bPrio[index]) return -1;
+						else if (aPrio[index] > bPrio[index]) return 1;
+						else index++;
+					}
+					return 0;
+				}
+			),
+		};
+	});
+
+	return {
+		...section,
+		categoryCombos,
+	};
+}
+
+function getPrio(categoryCombo, category) {
+	const categories = categoryCombo.categories.flat();
+	return categories.indexOf(category);
 }
