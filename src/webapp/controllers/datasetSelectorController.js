f5da377c836820ef426f54db94c0817f85dff4fd
p3rcypj <pablo@percypj.com>
2022-12-20 01:04:52 +0000

    Minor refactor + prettier

--- src/webapp/controllers/datasetSelectorController.js
+++ src/webapp/controllers/datasetSelectorController.js
@@ -2,111 +2,103 @@ import { TallySheets } from "../TallySheets.js";
 
 export const datasetSelectorController = TallySheets.controller(
 	"datasetSelectorCtrl",
-	[
-		"$scope",
-		"$rootScope",
-		"DataSetsUID",
-		"Locales",
-		function ($scope, $rootScope, DataSetsUID, Locales) {
-			$scope.id = "dsSelector" + $scope.selectorId;
-
-			$scope.selectorLoaded = false;
-
-			Locales.get().$promise.then(function (result) {
-				$scope.languages = result;
-			});
-
-			DataSetsUID.get().$promise.then(function (result) {
-				$scope.dataSetList = result.dataSets.filter((ds) => {
-					var visible = true;
-
-					for (att in ds.attributeValues) {
-						if (
-							ds.attributeValues[att].value == "true" &&
-							ds.attributeValues[att].attribute.id ==
-								"Re2UlY7OGO4"
-						) {
-							visible = false;
-						}
-					}
-					return visible;
-				});
-			});
-
-			$scope.$on("ngRepeatFinished", function (ngRepeatFinishedEvent) {
-				// Refresh bootstrap-select
-				$(".selectpicker").selectpicker("refresh");
-				$(".selectpicker").selectpicker("render");
-				$scope.selectorLoaded = true;
-			});
-
-			$scope.selected = [];
-
-			var datasetForm = document.querySelector("#datasetSelector");
-			$scope.selectedIds = [];
-			$(datasetForm).on("change", "#" + $scope.id, function () {
-				var formData = new FormData(datasetForm);
-				var selectedValues = formData.getAll("dataset");
-
-				var dsId = selectedValues.find(
-					(selected) => !$scope.selectedIds.includes(selected)
-				);
-				var dsName = $scope.dataSetList.find(
-					(dataSet) => dataSet.id === dsId
-				)?.displayName;
-
-				var languageList = $scope.dataSetList
-					.find((dataSet) => dataSet.id === dsId)
-					?.translations?.filter(
-						(translation) => translation.property === "NAME"
-					);
-
-				$scope.languageList = $scope.languages.filter((lang) =>
-					languageList?.some(
-						(language) =>
-							language.locale.split("_")[0] === lang.locale
-					)
-				);
-
-				$scope.selected = selectedValues;
-
-				$scope.bindToDataset.id = dsId;
-				$scope.bindToDataset.name = dsName;
-				$scope.bindToDataset.selectedIds = selectedValues;
-				$scope.bindToDataset.selected = $scope.selected.filter(Boolean);
-
-				$scope.selectedIds = selectedValues;
-
-				const exportButton = document.getElementById("export-button");
-				if (exportButton) {
-					if (_.isEmpty(selectedValues)) exportButton.disabled = true;
-					else exportButton.disabled = false;
-				}
-
-				$rootScope.$apply();
-			});
-
-			$scope.selectedLangs = [];
-			var languageForm = document.querySelector("#languageSelector");
-			$(languageForm).on("change", "#" + $scope.id, function () {
-				var formData = new FormData(languageForm);
-				var selectedValues = formData.getAll("language");
-
-				var selectedLanguage = selectedValues.find(
-					(selected) =>
-						!$scope.selectedLangs.includes(selected) &&
-						selected !== "en"
-				);
-
-				if (selectedLanguage !== undefined) {
-					$scope.selected.push(selectedLanguage);
-					$scope.bindToDataset.selected =
-						$scope.selected.filter(Boolean);
-				}
-
-				$scope.selectedLangs = selectedValues;
-				$rootScope.$apply();
-			});
-		},
-	]
+	["$scope", "$rootScope", "DataSetsUID", "Locales", controller]
 );
+
+function controller($scope, $rootScope, DataSetsUID, Locales) {
+	$scope.id = "dsSelector" + $scope.selectorId;
+
+	$scope.selectorLoaded = false;
+
+	Locales.get().$promise.then(function (result) {
+		$scope.languages = result;
+	});
+
+	DataSetsUID.get().$promise.then(function (result) {
+		$scope.dataSetList = result.dataSets.filter((ds) => {
+			var visible = true;
+
+			for (att in ds.attributeValues) {
+				if (
+					ds.attributeValues[att].value == "true" &&
+					ds.attributeValues[att].attribute.id == "Re2UlY7OGO4"
+				) {
+					visible = false;
+				}
+			}
+			return visible;
+		});
+	});
+
+	$scope.$on("ngRepeatFinished", function (ngRepeatFinishedEvent) {
+		// Refresh bootstrap-select
+		$(".selectpicker").selectpicker("refresh");
+		$(".selectpicker").selectpicker("render");
+		$scope.selectorLoaded = true;
+	});
+
+	$scope.selected = [];
+
+	var datasetForm = document.querySelector("#datasetSelector");
+	$scope.selectedIds = [];
+	$(datasetForm).on("change", "#" + $scope.id, function () {
+		var formData = new FormData(datasetForm);
+		var selectedValues = formData.getAll("dataset");
+
+		var dsId = selectedValues.find(
+			(selected) => !$scope.selectedIds.includes(selected)
+		);
+		var dsName = $scope.dataSetList.find(
+			(dataSet) => dataSet.id === dsId
+		)?.displayName;
+
+		var languageList = $scope.dataSetList
+			.find((dataSet) => dataSet.id === dsId)
+			?.translations?.filter(
+				(translation) => translation.property === "NAME"
+			);
+
+		$scope.languageList = $scope.languages.filter((lang) =>
+			languageList?.some(
+				(language) => language.locale.split("_")[0] === lang.locale
+			)
+		);
+
+		$scope.selected = selectedValues;
+
+		$scope.bindToDataset.id = dsId;
+		$scope.bindToDataset.name = dsName;
+		$scope.bindToDataset.selectedIds = selectedValues;
+		$scope.bindToDataset.selected = $scope.selected.filter(Boolean);
+
+		$scope.selectedIds = selectedValues;
+
+		const exportButton = document.getElementById("export-button");
+		if (exportButton) {
+			if (_.isEmpty(selectedValues)) exportButton.disabled = true;
+			else exportButton.disabled = false;
+		}
+
+		$rootScope.$apply();
+	});
+
+	$scope.selectedLangs = [];
+	var languageForm = document.querySelector("#languageSelector");
+	$(languageForm).on("change", "#" + $scope.id, function () {
+		var formData = new FormData(languageForm);
+		var selectedValues = formData.getAll("language");
+
+		var selectedLanguage = selectedValues.find(
+			(selected) =>
+				!$scope.selectedLangs.includes(selected) && selected !== "en"
+		);
+
+		if (selectedLanguage !== undefined) {
+			$scope.selected.push(selectedLanguage);
+			$scope.bindToDataset.selected = $scope.selected.filter(Boolean);
+		}
+
+		$scope.selectedLangs = selectedValues;
+		$rootScope.$apply();
+	});
+}
